4ae0bd1ac3a4e143b50c9ad3e50ced8cb9b0622b
indrol12 <indrolofficial@gmail.com>
2026-02-19 17:16:38 +0530

    added student login page

---
+++ src/student_lms/studentlogin.java
@@ -0,0 +1,15 @@
+package student_lms;
+
+public class studentlogin {
+public static void main(String[] args) {
+boolean username=true;
+boolean password=true;
+if(username==true && password==true)
+{
+	System.out.println("login successfull");
+}
+else {
+	System.out.println("login unsuccessfull");
+}
+}
+}
